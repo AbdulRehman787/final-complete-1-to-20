8b212920ac7401006b53a4e585f83567916c0c5c
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2021-12-03 23:18:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,926 @@
+// alert("Hello World");
+
+// alert("Hello MrBean \n How r you ")
+
+
+// console.log("Hello MrBean \n How r you ")
+
+
+// console.log("alert")
+
+// var a = "Hello ";
+// var b = "World";
+// var c = a+b;
+// alert(c)
+
+// document.write("A smarter way to learn Javascript")
+
+// var num = 2;
+// alert(num )
+
+// var age = 18;
+// alert("My Age is " +age)
+
+// document.write("30 "+ "June " +"2003")
+
+
+// var aname = "Abdul Rehman "
+// console.log(aname)
+
+// var $name = "Abdul Rehman "
+// console.log($name)
+
+
+// var $name = "Abdul Rehman "
+// console.log(name)
+
+// var @name = "Abdul Rehman "
+// console.log(aname)
+
+// var name = "Abdul Rehman "
+// console.log(aname)
+
+// var a = 3;
+// var b = 5;
+// var c = a+b;
+// console.log(c)
+
+
+// var a = 3;
+// var b = 5;
+// var c = b-a;
+// console.log(c)
+
+
+
+// var a = 3;
+// var b = 5;
+// var c = b*a;
+// console.log(c)
+
+
+
+// var a = 5;
+// var b = 5;
+// var c = b/a;
+// console.log(c)
+
+
+// var a = 3;
+// var b = 5;
+// var c = b%a;
+// console.log(c)
+
+
+
+// var num = 5;
+// var b = ++num - ++num + ++num - --num;
+// console.log(b)
+
+// var ticket = 600;
+// var totalTicket = 5;
+// var findResult = ticket * totalTicket;
+// console.log( "Total Cost to buy Ticket 5 is equal to" + findResult);
+
+
+// document.write("Table of 4 \n" +"<br>");
+// document.write(4 + " " + "* " + 1 + "="+ " " + 4 +"<br>" )
+// document.write(4 + " " + "* " + 2 + "="+ " " + 8 +"<br>")
+// document.write(4 + " " + "* " + 3 + "="+ " " + 12 +"<br>")
+// document.write(4 + " " + "* " + 4 + "="+ " " + 16 +"<br>" )
+// document.write(4 + " " + "* " + 5 + "="+ " " + 20  +"<br>")
+// document.write(4 + " " + "* " + 6 + "="+ " " + 24  +"<br>")
+// document.write(4 + " " + "* " + 7 + "="+ " " + 28  +"<br>")
+// document.write(4 + " " + "* " + 8 + "="+ " " + 32  +"<br>")
+// document.write(4 + " " + "* " + 9 + "="+ " " + 36 +"<br>" )
+// document.write(4 + " " + "* " + 10 + "="+ " " + 40  +"<br>")
+
+//  var priceofitem = 650;
+// var quantityofitem1 = priceofitem * 3;
+// var priceofitem = 100;
+// var quantityofitem2 =  priceofitem * 7;
+// var charges = 100;
+// console.log("total of your order is " ,quantityofitem1 + quantityofitem2 +100) 
+
+
+// var inp = prompt("Enter your city name");
+// inp = inp.lowercase
+// var cities = ["Karachi ", "Lahore" ,"Quetta" ,"Multan "]
+// if(cities === "Karachi"){
+//     alert(cities + "is the cleanist city ")
+// }
+
+
+
+// document.write("<h1>Total Curency in Pkr</h1>")
+// var dollar = 10;
+// var totalDollar = dollar * 104.80;
+// var riyal = 25;
+// var totalRiyal = riyal *25;
+// console.lod(totalDollar + totalRiyal)
+
+
+// var num = 5 * 10 / 2;
+// console.log(num)
+
+
+// var currentYear = 2021;
+// var birthYear = 2003;
+// var age = currentYear - birthYear ;
+// console.log(age)
+
+
+// var a  =  10;
+// var b = a + ++a  - a++  - --a + ++a;
+// console.log(b) 
+
+// var inp =prompt("Enter your name")
+// console.log(inp)
+
+// var inp = prompt("Enter the number");
+// for(var i = 1; i <=10;i++ ){
+//     document.write(inp + "x" + i + "=" + inp*i + "<br>")
+// }
+
+// var inp = prompt("Enter your city name ")
+// if(inp = "Karachi "){
+//     alert("Welcme to the city of light")
+// }
+
+// var gender = prompt("Enter your gender");
+
+// if(gender =="Male"){
+//     alert("Good Morning Sir")
+// } else if(gender =="Female"){
+//     alert("Good Morning MaM")
+// }else{
+//     alert("Type your gender ")
+// }
+
+// var input = prompt("Ënter the signal color");
+// if(input === "Red"){
+//     alert("Must Stop!");
+// }else if(input ==="Green"){
+//     alert("Move Away");
+// }else if(input ==="Orange"){
+//     alert("Ready To Move ");
+// }else{
+//     alert("Type the Signal color")
+// }
+
+
+// var input= prompt("Enter the remaining fuel in your car ");
+// if(input ==="0.25"){
+//     alert("Go to Fuel Pump And buy he fuel ! .")
+// }
+// else{
+//     alert("Enter the remaining fuel in your car")
+// }
+
+// var a = 4;
+// if(++a === 5){
+//     alert("Given Condition of A is true")
+// }
+
+
+// var materialCost = 20000;
+// var laborcost = 2000;
+// var totalCost = materialCost + laborcost;
+// if(totalCost === laborcost +materialCost){
+//     alert("The Cost Equal ")
+// }
+
+// if(true){
+//     alert("True")
+// }
+
+// if("Car" < "Cat"){
+//     alert(
+//         "Car is smaller than cat"
+//     )
+// }
+
+// var totalMark = 300;
+// var english = prompt("Enter your English Mark");
+// var Urdu = prompt("Enter your Urdu Mark");
+// var Math = prompt("Enter your Math Mark");
+// var Islamiat = prompt("Enter your Islamiat Mark");
+// var Physics = prompt("Enter your Physics Mark");
+// var Chemistry  = prompt("Enter your Chemistry  Mark");
+// var obtMark = english + Urdu +Math +Islamiat +Physics+Chemistry;
+// var totalMark = 300;
+// var percentage = obtMark / totalMark *100;
+// console.log(percentage)
+
+// var Number = prompt("Enter the any one Number and play this game") ;
+// if(Number ==2){
+//     alert("You Are win ")
+// }
+// else{
+//     alert("You Are Lost  this Match")
+// }
+
+// var number = prompt("Enter a number check by disibleb 3 ")
+// if(number %3==0){
+//     alert("This Number is divisible by 3 ")
+// }
+// else{
+//     alert("This Number is not divisible by 3")
+// }
+
+// var input= prompt("Enter the anyone number check this is even Number and Odd Number")
+// if(input %2 ==0){
+//     alert("This is even Number")
+// } else{
+//     alert("This is odd Number")
+// }
+
+// var temperature = prompt("Ënter the your city Temperature")
+// if(temperature >= 30 ){
+//     alert("“It is too hot outside.”")
+// }else if(temperature >=20){
+//     alert("“The Weather today is Normal.”")
+// } else if(temperature >= 10){
+//     alert("“Today’s Weather is cool.”")
+// }else if(temperature > 0){
+//     alert("“OMG! Today’s weather is so Cool.”")
+// }
+
+
+// var inp =prompt("Enter the city Name")
+// if(inp == "Karachi"){
+//     alert("Correct Answer")
+// }
+// else(alert("Karachi aja phir dekhty hy "))
+
+// var inp = prompt("Enter the city Name")
+// if(inp ==="Karachi"){
+//     alert("Correct Answer")
+// }else if(inp ==="lyari"){
+//     alert("Incorrect But try Agian")
+// }
+// else{
+//     alert("Karachi aja phir dekhty hy ")
+// }
+// if((x===y || a === b)&&(a===b)){
+
+// }
+
+
+// var charint = prompt("Enter a character ")
+// if(charint >="A" &&  charint <= "Z" ){
+//     alert("This is upper case")
+// }
+// else if(charint >="a" && charint <="z"){
+//     alert("This is lower case ")
+// }else{
+//     alert("This is not a character")
+// }
+
+// var int = prompt("Enter a value 0 to 100 ");
+// var int1 = prompt("Enter a value 0 to 100 ");
+
+
+// if(int === int1){
+//     alert("this is integar is equal to  another integaar")
+// }
+// else{
+//     alert("this is not eaual ")
+// }
+
+
+// var input = prompt("Enter a Number ")
+// if(input>0){
+//     alert("this is positive number")
+// }else if(input<0){
+//     alert("This is negative number")
+// }
+// else{
+//     alert("Please reload the page ")
+// }
+
+
+// var input=prompt("Enter a password");
+// var password = "A.Rehman";
+// if(input === password){
+//     alert("Correct Password")
+// }
+// else{
+//     alert("Incorrect Password")
+// }
+
+
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// alert(greeting);
+// }
+// else{
+// greeting = "Good evening";
+// alert("greeting")
+// }
+
+// var user = prompt("Enter the number")
+// if((user =="Abdul Rehman")&&(user =="Aslam")){
+//     alert("Your  data is supported on google")
+// }else(
+//     alert("Sorry User")
+// )
+
+
+
+// var arr = []
+// arr[0]="Abdul Rehman"
+// console.log(arr)
+
+// var arr = [];
+// arr[0]="Abdul ";
+// arr[1]="Rehman";
+// console.log(arr)
+
+// var user =prompt("Enter a city Name")
+// if(user ==="Karachi"){
+//     alert("Welcomethe city oglight")
+// }
+
+
+// var x = "Karachi"
+// if(x==="Karachi"){
+//     alert("Hello Karachi Walon")
+// }
+
+var correctAnswer = "Vatican";
+//  if (x === correctAnswer) {
+// score++;
+// userIQ = "genius";
+// alert("Correct!");
+// }
+
+
+// console.log(0>=0)
+
+// var num = 2;
+// num ++;
+// ++num;
+// console.log(num--)
+// console.log( num++)
+
+// var inp = prompt("Enter your city name");
+// inp = inp.toLowerCase();
+// var cityName = ["Karachi ","Lahore","Multan","Peshawar" ,"Lyari","ChilChock "]
+// for(var i =0; i <= 4;  i++){
+//     if(inp === cityName[i]){
+//         alert("This is city is clean")
+//     }
+// }
+
+// var cityToCheck = prompt("Enter your city");
+// cityToCheck = cityToCheck.toLowerCase();
+// var cleanestCities = ["cheyenne", "santa fe", "tucson", "great falls", "honolulu"];
+// for (var i = 0; i <= 4; i++) {
+// if (cityToCheck === cleanestCities[i]) {
+//     alert("It's one of the cleanest cities");
+// }
+//  }
+
+// var num = +prompt("Enter a number");
+// if(!isNaN(num)){
+//     if(num%2==0){
+    
+//         alert("This is Even number")
+//     }
+//     else{
+//         alert("this is odd number")
+//     }
+    
+// }else{
+//     console.log("This is not a Nunber")
+// }
+
+// var num = +prompt("Enter a number");
+// if(num %3 ==0 && num%5==0){
+//     console.log("fizz buzz")
+    
+// } else if(num % 3==0){
+//     console.log("fizz")
+// }
+// else if(num % 5 ==0){
+//     console.log("buzz")
+// }
+
+// else{
+//     console.log(num)
+// }
+
+
+
+// var int1 = +prompt("Enter a number and check the integar");
+// var int2 = +prompt("Enter a number and check the integar")
+// if(int1===int2){
+//     alert("Integar is equal to another integar")
+// }
+// else{
+//     alert("This is not equal to integar")
+// }
+
+
+// var num =+prompt("Enter the number and check the its positive then negative")    
+// if(num>0){
+//     console.log("it is positive")
+// }else if (num<0){
+//     console.log("it is Negative")
+// }
+// else{
+//     console.log("it is Zero")
+// }
+
+// var vowelChar = prompt("Enter a character to check whether it is vowel or not");
+
+// if(vowelChar === "A" || vowelChar === "E" || vowelChar === "I" || vowelChar === "O" || vowelChar === "U"
+//     || vowelChar === "a" || vowelChar === "e" || vowelChar === "i" || vowelChar === "o" || vowelChar === "u"){
+//     alert(vowelChar + " " + "is a vowel");
+// }
+// else{
+//     alert(vowelChar + " " + "is not a vowel");
+// }
+
+// var password = "Noor0786"
+// var inp = prompt("Enter the your Password");
+// if(inp ==password){
+//     console.log("Correct Password")
+// }else{
+//     console.log("Incorrect Password")
+// }
+
+// var arr =[]
+// arr[0]="Abdul";
+// arr[1]="Rehman";
+// console.log(arr)
+
+
+// var arr=["abdul rehman"];
+// console.log(arr)
+// typeof[arr]
+
+// var arr =[1,2,3,4,5,6,7,8,9]
+// console.log(arr)
+
+// var arr=["abdul rehman ", 1,2,3,4,5,6,7,8,9]
+// console.log(arr)
+
+// var mixArry = ["SSC"    + "<br>", "HSC"  + "<br>","BCS"  + "<br>","BS"  + "<br>","BCOM"  + "<br>","MS"  + "<br>" ,"M.PHIL"  + "<br>" ,"MASTER"  + "<br>","PHD"]
+// document.write(mixArry + "<br>") 
+
+
+//pdf chapter 14 t0 16 assement task 8
+//var score1 = ["Score of Michel","Score ofJhon","Score of Ton"]
+//var totalmark = 500;
+//var obtmark=prompt("Enter the your Matric Mark")
+//var score = obtmark/totalmark%100;
+//if(obtmark>=421 && score){
+//    console.log(score1[0] + " " + obtmark+ " " +["is"] + " "+ " " +["percentage of Michal is  "] +" "+ score)
+//} 
+// else if (obtmark>=399 && score){
+//     console.log(score1[1]+ " " + obtmark + " "+["is"] + " " + " " +["percentage of Jhon is "]+ " " + score)
+// }
+// else if(obtmark >=362 && score){
+//     console.log(score1[2]+ " " + obtmark + " "+["is"] + " " + " " +[ "percentage of ton is "] + " " + score)
+// }
+
+
+
+
+
+
+
+// var arr =[222,322,129,233,245]
+// // console.log(arr)
+// var cities = ["Karachi","Lahore" ,"Islamabad","Quetta","Peshawar"]
+// cities.splice[0,1,3];
+// console.log(cities)
+ 
+// for(var i =1; i < 10 ; i++){
+//     console.log( "Hello World",i )
+// }
+
+// for(var i = 0;i<5;i++){
+//     console.log("Hello World ")
+// }
+
+// for(var i = 0;i<5;i++){
+//     document.write("Hello World"  +"<br>")
+// }
+
+// var inp = prompt("Enter a Number");
+// for(var i=1;i<=10;i++){
+//     console.log(inp + " " + "X"+ " " + i + " = " +inp*i )
+// }
+
+// var arr = ["Abdul","Rehman","Muhammad","Aslam"];
+// for(var i = 0 ; i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+// var arr=[222,333,555,666]
+// for(var i = 0 ;i<arr.length  i++){
+//     console.log(arr[i])
+// }
+
+// var arr = ["Karachi","Lahore","Peshawar","Quetta"]
+// for(var i = 0; i<arr.length;i++){
+//     if(arr[i] ==="Karachi"){
+//         alert( arr[i] +"is the Cleanist city")
+//     }
+    
+// }
+
+// var arr = ["Karachi","Lahore","Peshawar","Quetta"]
+// for(var i = 0; i<arr.length;i++){
+    
+//         alert( arr[i] +" is the Cleanist city")
+    
+    
+// }
+
+
+
+// for(var i =1 ;i>0;i++){
+//     console.log("Hello User")
+// }
+
+// var cities = ["Karachi","Lahore","Multan","Islamabad"]
+// for(var i=0 ;i<cities.length;i++){
+//     if(cities[i]==="Lahore"){
+//         console.log(cities[i] ,"Is the cleanist city")
+//         break
+//     }
+    
+// }
+
+// var firstName = ["Abdul ","Rehman" ,"A.Rehman"];
+// var lastName = ["Muhammad","Aslam" ]
+// for(var i =0;i<firstName.length;i++){
+//     for(var j=0;j<lastName.length;j++){
+//         console.log(firstName[i]," ", lastName[j])
+//     }
+// }
+
+
+// for(var i =0;i<10;i++){
+//     for(var j =0;j<i;j++){
+//         document.write("*")
+//     }
+   
+//         document.write("<br>")
+// }
+
+// var word = "civic";
+// for(var i=0;i<word;i++){
+//     for(var j = 0;j<word ;j++){
+//         document.write("it is pap word")
+//     }
+//     document.write("it is not pad word")
+// }
+// var word = prompt("Enter a word");
+// var check="";
+// for(var i = word.length-1; i>=0 ; i--){
+//   check+= word[i]
+// }
+// //console.log(word,check)
+
+// if(word===check){
+//     console.log(word ,"is the palindrom word")
+// }else{
+//     console.log("it is not parandolm word")
+// }
+
+// var inp = prompt("Enter a word");
+// var check="";
+// for(var i =inp.length-1;i>=0;i--){
+//    check+=inp[i]
+// }
+// // console.log(check,word)
+// if(inp===check){
+//   console.log(inp +" It is a palindram word")
+// }
+// else{
+//   console.log("It is not a paralindram word")
+// }
+// var firstName=["Abdul","Rehman","A.Rehman"];
+// var lastName = ["Muhammad Aslam"]
+// for(var i=0 ;i<firstName.length;i++){
+//   for(var j=0;j<lastName.length;j++){
+//     console.log(firstName[i],  " " , lastName[j])
+//   }
+// }
+
+
+// var firstName = ["Abdul ","Rehman" ,"A.Rehman"];
+// var lastName = ["Muhammad","Aslam" ]
+// for(var i =0;i<firstName.length;i++){
+//     for(var j=0;j<lastName.length;j++){
+//         console.log(firstName[i]," ", lastName[j])
+//     }
+// }
+// var firstName = ["Abdul ","Rehman","A.Rehman"];
+// var lastName = ["Muhammad Aslam"]
+// for(var i = 0;i<firstName.length;i++){
+//   for(var j = 0;j<lastName.length;j++){
+//     console.log(firstName[i]," ",lastName[j])
+//   }
+// }
+
+
+// var Name = "Abdul Rehman";
+// console.log(Name.indexof("R"))
+// 
+//var name="Ghous Ahmed";
+//console.log(name.indexOf("A"))
+// var name = 'AbdulRehman';
+// console.log(name.indexOf("R"))
+
+// var val1= +prompt("Enter a first Value ")
+// var val2= +prompt("Enter a Second Value")
+// var sign= prompt("Enter a Operater")
+
+// for(var i=0;i<val1;i++){
+//   for(var j = 0;j<val2;j++){
+//     if(sign === "+" &sign==="-" &sign==="*" &sign==="/"){
+//       console.log(val1 +sign+ val2, )
+//     }
+    
+//   }
+// }
+var val1= +prompt("Enter a first Value ")
+var sign= prompt("Enter a Operater");
+var val2= +prompt("Enter a Second Value")
+
+console.log(val1 , sign ,val2)
+if(sign === "+"){
+  alert(val1 + val2 )
+}else if(sign==="-"){
+  alert(val1 - val2 )
+}else if(sign==="*"){
+  alert(val1 * val2 )
+} else if(sign==="/"){
+  alert(val1 / val2 )
+}
+else if(sign==="%"){
+  alert(val1 / val2 *100 )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
